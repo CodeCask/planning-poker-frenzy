5bb079bcf92cff4a1040c72fd658825caccdc84f
David Sherman <thedcsherman@gmail.com>
2022-11-14 21:43:06 +0100

    add vote to tracking

--- sveltekit/src/lib/stores/users.ts
+++ sveltekit/src/lib/stores/users.ts
@@ -1,9 +1,10 @@
 import { supabaseClient } from '$lib/db';
 import { page } from '$app/stores';
 import { browser } from '$app/environment';
-import { derived, readable, writable, type Readable } from 'svelte/store';
+import { derived, readable, writable, type Readable, type Unsubscriber } from 'svelte/store';
+import { vote } from '$lib/stores/vote';
 
-type Presence = { presence_ref: string };
+type Presence = { presence_ref: string; vote: number | null };
 
 export function createOnlineUsersStore(roomId?: string, email?: string) {
 	if (!browser || roomId == null || email == null) return readable<Presence[]>([]);
@@ -16,9 +17,16 @@ export function createOnlineUsersStore(roomId?: string, email?: string) {
 		}
 	});
 
+	let unsubscribe: Unsubscriber;
+
 	const { subscribe, update } = writable<Presence[]>([], () => {
 		return () => {
 			console.log(`Unsubscribing from online users for room ${roomId}`);
+
+			if (unsubscribe) {
+				unsubscribe();
+			}
+
 			return channel.unsubscribe();
 		};
 	});
@@ -29,7 +37,7 @@ export function createOnlineUsersStore(roomId?: string, email?: string) {
 
 	channel.on('presence', { event: 'join' }, ({ newPresences }) => {
 		console.log('New users have joined: ', newPresences);
-		update((users) => [...users, ...newPresences]);
+		update((users) => [...users, ...(newPresences as Presence[])]);
 	});
 
 	channel.on('presence', { event: 'leave' }, ({ leftPresences }) => {
@@ -40,9 +48,11 @@ export function createOnlineUsersStore(roomId?: string, email?: string) {
 	});
 
 	console.log(`Subscribing to online users for room ${roomId}`);
-	channel.subscribe(async (status, err) => {
-		if (status === 'CHANNEL_ERROR') {
-			console.log(err);
+	channel.subscribe(async (status) => {
+		if (status === 'SUBSCRIBED') {
+			unsubscribe = vote.subscribe((vote) => {
+				channel.track({ vote });
+			});
 		}
 	});
 
